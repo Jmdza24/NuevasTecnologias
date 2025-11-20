6a213255ffeff11b4273a1384173356f45d44724
Joan Mendoza <sjoan575@gmail.com>
2025-11-20 05:45:09 -0500

    Actualizacion de test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,12 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_redirects_to_login(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302); // Redirección
+        $response->assertRedirect('/login');
     }
+
 }
